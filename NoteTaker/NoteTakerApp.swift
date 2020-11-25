cd980fe2715cdf1b2eb1d98b94bb7eccd1f3e8d1
Katie Embrey-Farquhar <kembreyfarquhar@gmail.com>
2020-11-24 18:27:24 -0600

    Initial Commit

---
+++ NoteTaker/NoteTakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakerApp.swift
+//  NoteTaker
+//
+//  Created by Katie on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct NoteTakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
